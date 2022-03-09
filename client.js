ad828035c2120493c03939533cf80f2d709dab9b
LukasKlab <lukas.bendiksen@gmail.com>
2022-03-09 09:30:49 +0100

    ws to wss

--- client.js
+++ client.js
@@ -16,7 +16,7 @@ function init() {
     ws.close();
   }
 
-  ws = new WebSocket("ws://labindustries.net:443");
+  ws = new WebSocket("wss://labindustries.net:443");
   ws.onopen = () => {
     console.log("Connection opened!");
   };
